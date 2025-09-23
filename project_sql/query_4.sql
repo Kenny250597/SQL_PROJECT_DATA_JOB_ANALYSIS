2911589b226602645d0ebdeb95225a856e52a523
Kenny250597 <sureshnallan25@gmail.com>
2025-09-23 17:27:33 +0800

    push_query_4_5

--- project_sql/query_4.sql
+++ project_sql/query_4.sql
@@ -5,3 +5,22 @@ QUERY 4 - top skills based on salary for my role
 
 */
 
+
+SELECT
+    skills_dim.skills,
+    ROUND(AVG(job_table.salary_year_avg),0) AS avg_salary
+FROM job_postings_fact AS job_table
+
+INNER JOIN skills_job_dim AS skill ON job_table.job_id = skill.job_id
+INNER JOIN skills_dim ON skill.skill_id = skills_dim.skill_id
+
+WHERE
+    job_table.job_title_short = 'Data Analyst' AND
+    job_table.salary_year_avg IS NOT NULL AND
+    job_table.job_work_from_home = True
+
+GROUP BY skills
+
+ORDER BY avg_salary DESC
+
+LIMIT 25
---
+++ project_sql/query_5.sql
@@ -0,0 +1,6 @@
+/*
+
+QUERY 4 - most optimal skills to learn (high demand and high paying)
+
+*/
+
